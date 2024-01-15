28e98ed68efb07adaf03329a8ea9c92ef1558631
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-01-15 00:45:43 -0500

    [add][fix] Refactor userSchema in userModel.ts

--- src/api/models/userModel.ts
+++ src/api/models/userModel.ts
@@ -24,13 +24,6 @@ const userSchema: Schema<IUser> = new mongoose.Schema({
     }]
 });
 
-userSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 8);
-    }
-    next();
-});
-
 const User = mongoose.model<IUser>('User', userSchema);
 
 export default User;
