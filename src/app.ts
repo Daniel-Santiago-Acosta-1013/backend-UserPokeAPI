eab36f3f4a699a72fe9a22d03f5256ef12470e5a
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-01-14 21:46:37 -0500

    [add][fix] Add CORS support for whitelist domains

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
 import connectDB from './config/db';
 import userRoutes from './api/routes/userRoutes';
 import { errorHandler } from './utils/errorHandler';
 
 const app = express();
+
+// Lista blanca de dominios
+const whitelist: string[] = ['http://localhost:5173'];
+
+// Opciones de CORS
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin ?? '') || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to the database
